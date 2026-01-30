bc503deba9fb71c537088c5e465ba1b8fd6dd6e0
Atharva Ahire <atharvahire2006@gmail.com>
2026-01-30 13:32:07 +0530

    error slove

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,18 +1,18 @@
-// // const { Sequelize } = require("@sequelize/core");
-// // const { PostgresDialect } = require("@sequelize/postgres");
+// const { Sequelize } = require("@sequelize/core");
+// const { PostgresDialect } = require("@sequelize/postgres");
 
-// const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   logging: false,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-// module.exports = sequelize;
+module.exports = sequelize;
