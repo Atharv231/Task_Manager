67f2672a84e4fa55ae11798111e09b3c66009d38
Atharva Ahire <atharvahire2006@gmail.com>
2026-02-11 20:57:05 +0530

    make chnages

--- frontend/Task_Manager/src/App.jsx
+++ frontend/Task_Manager/src/App.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import TaskList from "./components/TaskList";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
+// import React from "react";
+// import TaskList from "./components/TaskList";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import LoginPage from "./components/LoginPage";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/TaskList" element={<TaskList />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<LoginPage />} />
+//         <Route path="/TaskList" element={<TaskList />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
